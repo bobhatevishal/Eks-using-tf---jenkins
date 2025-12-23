7f5ab8f74f3918af8aa1216979bd4600c0969377
bobhatevishal <bobhatev13@gmail.com>
2025-12-23 12:50:54 +0000

    terraform

---
+++ terraform/provider.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  backend "s3" {
+    bucket = "backend-bucket13312662"
+    key    = "eks/terraform.tfstate"
+    region = "ap-south-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.29"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
